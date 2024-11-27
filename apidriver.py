f1cb440e71e238e745f2923ad7e68919f324555d
sergeihychko <shychko@gmail.com>
2024-11-27 02:49:33 -0500

    Added the functionality to find a single post, create a new post, delete a post by it's uri.

--- apidriver.py
+++ apidriver.py
@@ -14,6 +14,7 @@ class Driver:
 
         posts = client.app.bsky.feed.post.list(client.me.did, limit=10)
         for uri, post in posts.records.items():
+            print("retrieving post - uri : " + uri)
             print(uri, post.text)
 
 
@@ -33,6 +34,29 @@ class Driver:
         # new_post = client.app.bsky.feed.post.create(client.me.did, post_record)
         # print(new_post)
 
+    def find_single_skeep(self, account, token, uri):
+        client = Client()
+        client.login(account, token)
+        print("Calling feed.post.get")
+        post = client.app.bsky.feed.post.get(client.me.did, AtUri.from_str(uri).rkey)
+        print("Post text : " + post.value.text)
+
+    def create_skeet(self, account, token, skeet_text):
+        client = Client()
+        client.login(account, token)
+        post_record = models.AppBskyFeedPost.Record(text=skeet_text,
+                                                    created_at=client.get_current_time_iso())
+        new_post = client.app.bsky.feed.post.create(client.me.did, post_record)
+        print("Created new skeet with the text : " + post_record.text + " : the new uri :" + new_post.uri)
+        uri = new_post.uri
+        return uri
+
+    def delete_skeet(self, account, token, uri):
+        client = Client()
+        client.login(account, token)
+        print("Deleting the post at the uri : " + uri)
+        deleted_post = client.app.bsky.feed.post.delete(client.me.did, AtUri.from_str(uri).rkey)
+        print(deleted_post)
 
     def get_follow_authors(self, account, token, author):
         print(" fetching all the followers of an author")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from configparser import ConfigParser
 import os
 import apidriver
+import time
 import json
 
 account = ""
@@ -24,24 +25,36 @@ application_title = config.get('main-section', 'application_title')
 database_name = config.get('main-section', 'database_name')
 print("calling api driver :" + account + ":" + token + ":" + default_limit)
 driver_object = apidriver.Driver()
-driver_object.perform_get_skeets(account, token, default_limit)
+
+# get your last 10 skeets
+#driver_object.perform_get_skeets(account, token, default_limit)
 
 # print("calling get deleted posts")
 # driver_object.get_deleted(account, token)
 
-follows = driver_object.get_follow_authors(account, token, account)
-# for author in follows:
-#     print("author :" + str(author))
-#create json output file of followers
-filename = 'frequency.json'
-try:
-    os.remove(filename)
-except OSError:
-    pass
-print("writing followers list to jason file")
-with open(filename, 'w') as f:
-    json.dump(follows, f, indent=4)
+# Test creating a new skeet, query the skeet, then delete the skeet
+#new_skeet_uri = driver_object.create_skeet(account, token, "This a test skeet using an api call. It will be deleted momentarily")
+# print("sleep for 5.....")
+# time.sleep(5)
+# driver_object.find_single_skeep(account, token, new_skeet_uri)
+# time.sleep(5)
+# print("sleep for 5.....")
+# driver_object.delete_skeet(account, token, new_skeet_uri)
 
-print("ending TEST DRIVER")
+
+# follows = driver_object.get_follow_authors(account, token, account)
+#     # for author in follows:
+#     #     print("author :" + str(author))
+#     #create json output file of followers
+# filename = 'frequency.json'
+# try:
+#     os.remove(filename)
+# except OSError:
+#     pass
+# print("writing followers list to jason file")
+# with open(filename, 'w') as f:
+#     json.dump(follows, f, indent=4)
+#
+# print("ending TEST DRIVER")
 
 
